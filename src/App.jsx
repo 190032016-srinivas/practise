8df84f3e66cdefc416c8916c556755dc6601928b
190032016-srinivas <74003608+190032016-srinivas@users.noreply.github.com>
2025-05-17 23:09:38 +0530

    stopwatch

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [secs, setSecs] = useState(0);
+  const [play, setPlay] = useState(false);
+
+  useEffect(() => {
+    if (play) {
+      const id = setInterval(() => {
+        setSecs((x) => x + 1);
+      }, 1000);
+      return () => clearInterval(id);
+    }
+  }, [play]);
+
+  const formatTime = (s) => {
+    const hrs = Math.floor(s / 3600) % 24;
+    const mins = Math.floor(s / 60) % 60;
+    const secs = s % 60;
+    return (
+      hrs.toString().padStart(2, "0") +
+      ":" +
+      mins.toString().padStart(2, "0") +
+      ":" +
+      secs.toString().padStart(2, "0")
+    );
+  };
+
+  return (
+    <div className="App">
+      <h1>Stopwatch</h1>
+      <div className="time-display">{formatTime(secs)}</div>
+      <button onClick={() => setPlay(!play)}>{play ? "Stop" : "Start"}</button>
+      <button
+        onClick={() => {
+          setPlay(false);
+          setSecs(0);
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default App;
